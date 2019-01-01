ab43ea52bbf6ecf58e4b623a181b804e642a6c51
Johnmacrocraft <Johnmacrocraft@users.noreply.github.com>
2019-01-01 09:42:11 +0900

    Bump copyright date, make use of anonymous function for rmdirAll

--- src/Johnmacrocraft/NoTmpContent/NoTmpContent.php
+++ src/Johnmacrocraft/NoTmpContent/NoTmpContent.php
@@ -4,7 +4,7 @@
  *
  * NoTmpContent
  *
- * Copyright © 2017-2018 Johnmacrocraft
+ * Copyright © 2017-2019 Johnmacrocraft
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,28 +23,26 @@ class NoTmpContent extends PluginBase {
 	public function onDisable() : void {
 		if(file_exists($this->getServer()->getDataPath() . "tmp")) {
 			$this->getLogger()->info(TextFormat::GOLD . "Cleaning tmp folder...");
-			$this->rmdirAll($this->getServer()->getDataPath() . "tmp");
+
+			$rmdirAll = function(string $dir) use(&$rmdirAll) {
+				$dirs = dir($dir);
+				while(false !== ($entry = $dirs->read())) {
+					if(($entry != ".") && ($entry != "..")) {
+						if(is_dir($dir . "/" . $entry)) {
+							$rmdirAll($dir . "/" . $entry);
+							@rmdir($dir . "/" . $entry);
+						} else {
+							@unlink($dir . "/" . $entry);
+						}
+					}
+				}
+				$dirs->close();
+			};
+			$rmdirAll($this->getServer()->getDataPath() . "tmp");
+
 			$this->getLogger()->info(TextFormat::GREEN . "Done!");
 		} else {
 			$this->getLogger()->info(TextFormat::RED . "NoTmpContent didn't clear tmp folder because it doesn't exist. Seems like you're not using PocketMine-MP for Android or deleted it yourself?");
 		}
-	}
-
-	/*
-	 * @param string $dir
-	 */
-	private function rmdirAll(string $dir) : void {
-		$dirs = dir($dir);
-		while(false !== ($entry = $dirs->read())) {
-			if(($entry != ".") && ($entry != "..")) {
-				if(is_dir($dir . "/" . $entry)) {
-					$this->rmdirAll($dir . "/" . $entry);
-					@rmdir($dir . "/" . $entry);
-				} else {
-					@unlink($dir . "/" . $entry);
-				}
-			}
-		}
-	$dirs->close();
 	}
 }
